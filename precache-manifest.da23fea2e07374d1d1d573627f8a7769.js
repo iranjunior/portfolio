f3993558f7ee42ccaed80998a979b714fb0224b9
“Iran <iranjunior94@gmail.com>
2020-02-07 00:59:38 -0300

    Updates

---
+++ precache-manifest.da23fea2e07374d1d1d573627f8a7769.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f1ebb63cfa32672ec91d4e9b85619fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d1a1b40ac0db76e581b",
+    "url": "/static/js/2.4bbeffeb.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.4bbeffeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "139fe325e39256149b53",
+    "url": "/static/js/main.8ec2e2fc.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  }
+]);
