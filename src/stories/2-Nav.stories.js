6c126a5725a70de2b92a8c73636ab214f84d6f61
“Iran <iranjunior94@gmail.com>
2020-02-20 21:16:23 -0300

    feature(chromatic): stated cromatic

--- src/stories/2-Nav.stories.js
+++ src/stories/2-Nav.stories.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { action } from '@storybook/addon-actions';
 import Nav from '../components/nav';
 
 export default {
   title: 'Nav',
 };
 const mockHistory = {
-  push: (route) => console.log(route),
+  push: (route) => action(route),
 };
 const mockMatch = {
   path: '/',
